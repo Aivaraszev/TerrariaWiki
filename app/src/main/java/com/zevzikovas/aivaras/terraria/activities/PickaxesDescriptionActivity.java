6a81407380410833d4f672806d529c0391922a0c
Aivaraszev <aivaraszev@gmail.com>
2018-04-18 17:28:18 +0300

    XD

--- app/src/main/java/com/zevzikovas/aivaras/terraria/activities/PickaxesDescriptionActivity.java
+++ app/src/main/java/com/zevzikovas/aivaras/terraria/activities/PickaxesDescriptionActivity.java
@@ -1,21 +1,21 @@
-package com.zevzikovas.aivaras.terraria.activities;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.TextView;
-
-import com.zevzikovas.aivaras.terraria.R;
-
-public class PickaxesDescriptionActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pickaxes_description);
-
-        Intent i = getIntent();
-        TextView hswordsId = findViewById(R.id.pickaxesId);
-        hswordsId.setText(String.valueOf(i.getIntExtra("hswordsId", 0)));
-    }
-}
+//package com.zevzikovas.aivaras.terraria.activities;
+//
+//import android.app.Activity;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.widget.TextView;
+//
+//import com.zevzikovas.aivaras.terraria.R;
+//
+//public class PickaxesDescriptionActivity extends Activity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_pickaxes_description);
+//
+//        Intent i = getIntent();
+//        TextView hswordsId = findViewById(R.id.pickaxesId);
+//        hswordsId.setText(String.valueOf(i.getIntExtra("hswordsId", 0)));
+//    }
+//}
