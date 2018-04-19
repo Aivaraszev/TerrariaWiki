66d76ec4c91e9a2c0771c4f5b055ad980c26d9a0
Aivaraszev <aivaraszev@gmail.com>
2018-04-19 20:23:11 +0300

    XD

--- app/src/main/java/com/zevzikovas/aivaras/terraria/repositories/HSwordsRepository.java
+++ app/src/main/java/com/zevzikovas/aivaras/terraria/repositories/HSwordsRepository.java
@@ -83,7 +83,7 @@ public class HSwordsRepository {
         prepareHswords(db, "Meowmere", R.drawable.item_meowmere, 11, "White", 20);
     }
 
-    private void prepareHswords(SQLiteDatabase db, String name, int picture, int damage, String rarity, int price) {
+    private void prepareHswords(SQLiteDatabase db, String name, int picture, int damage, int knockback,  String rarity, int price) {
         ContentValues values = new ContentValues();
         values.put(NAME, name);
         values.put(PICTURE, picture);
