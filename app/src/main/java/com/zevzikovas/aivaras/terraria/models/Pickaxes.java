9d238bb31bd6d5c420adff9f7b51e0385844b46e
Aivaraszev <aivaraszev@gmail.com>
2018-04-20 21:18:52 +0300

    XDDDDD

--- app/src/main/java/com/zevzikovas/aivaras/terraria/models/Pickaxes.java
+++ app/src/main/java/com/zevzikovas/aivaras/terraria/models/Pickaxes.java
@@ -5,29 +5,47 @@ public class Pickaxes {
           public String name;
           public int picture;
           public int damage;
-          public int knockback;
+          public String knockback;
+          public String bonus;
+          public String critical_chanse;
+          public int use_time;
+          public int tool_speed;
           public String rarity;
-          public int price;
-          public int power;
+          public String buy_price;
+          public String sell_price;
+          public String pickaxe_power;
+          public String axe_power;
 
-    public Pickaxes (int id, String name, int picture, int damage, int knockback, String rarity, int price, int power) {
+    public Pickaxes (int id, String name, int picture, int damage, String knockback, String bonus, String critical_chanse, int use_time, int tool_speed, String rarity, String buy_price, String sell_price, String pickaxe_power, String axe_power) {
         this.id = id;
         this.name = name;
         this.picture = picture;
         this.damage = damage;
         this.knockback = knockback;
+        this.bonus = bonus;
+        this.critical_chanse = critical_chanse;
+        this.use_time = use_time;
+        this.tool_speed = tool_speed;
         this.rarity = rarity;
-        this.price = price;
-        this.power = power;
+        this.buy_price = buy_price;
+        this.sell_price = sell_price;
+        this.pickaxe_power = pickaxe_power;
+        this.axe_power = axe_power;
     }
 
-    public Pickaxes (String name, int picture, int damage, int knockback, String rarity, int price, int power) {
+    public Pickaxes (String name, int picture, int damage, String knockback, String bonus, String critical_chanse, int use_time, int tool_speed, String rarity, String buy_price, String sell_price, String pickaxe_power, String axe_power) {
         this.name = name;
         this.picture = picture;
         this.damage = damage;
         this.knockback = knockback;
+        this.bonus = bonus;
+        this.critical_chanse = critical_chanse;
+        this.use_time = use_time;
+        this.tool_speed = tool_speed;
         this.rarity = rarity;
-        this.price = price;
-        this.power = power;
+        this.buy_price = buy_price;
+        this.sell_price = sell_price;
+        this.pickaxe_power = pickaxe_power;
+        this.axe_power = axe_power;
     }
 }
--- app/src/main/java/com/zevzikovas/aivaras/terraria/repositories/PickaxesRepository.java
+++ app/src/main/java/com/zevzikovas/aivaras/terraria/repositories/PickaxesRepository.java
@@ -20,9 +20,15 @@ public class PickaxesRepository {
         private static final String PICTURE = "picture";
         private static final String DAMAGE = "damage";
         private static final String KNOCKBACK = "knockback";
+        private static final String BONUS = "bonus";
+        private static final String CRITICAL_CHANSE = "critical_chanse";
+        private static final String USE_TIME = "use time";
+        private static final String TOOL_SPEED = "tool_speed";
         private static final String RARITY = "rarity";
-        private static final String PRICE = "price";
-        private static final String POWER = "power";
+        private static final String BUY_PRICE = "buy_price";
+        private static final String SELL_PRICE = "sell_price";
+        private static final String PICKAXE_POWER = "pickaxe_power";
+        private static final String AXE_POWER = "axe_power";
         private SQLiteOpenHelper dbHelper;
 
 
@@ -35,10 +41,16 @@ public class PickaxesRepository {
                         NAME + " TEXT," +
                         PICTURE + " INTEGER," +
                         DAMAGE + " INTEGER," +
-                        KNOCKBACK + " INTEGER," +
+                        KNOCKBACK + " TEXT," +
+                        BONUS + " TEXT," +
+                        CRITICAL_CHANSE + " TEXT," +
+                        USE_TIME + " INTEGER," +
+                        TOOL_SPEED + " INTEGER" +
                         RARITY + " TEXT," +
-                        PRICE + " INTEGER" +
-                        POWER + "INTEGER" +
+                        BUY_PRICE + " TEXT" +
+                        SELL_PRICE + " TEXT" +
+                        PICKAXE_POWER + " TEXT" +
+                        AXE_POWER + " TEXT" +
                         ")"
         );
     }
@@ -50,47 +62,53 @@ public class PickaxesRepository {
     public void fill(SQLiteDatabase db) {
 
 
-        preparePickaxes(db, "Copper Pickaxe", R.drawable.item_copper_pickaxe, 11, 2,"White", 20 ,20);
-        preparePickaxes(db, "Tin Pickaxe", R.drawable.item_tin_pickaxe, 11, 2,"White", 20 ,20);
-        preparePickaxes(db, "Cactus Pickaxe", R.drawable.item_cactus_pickaxe, 11, 2,"White", 20 ,20);
-        preparePickaxes(db, "Iron Pickaxe", R.drawable.item_iron_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Lead Pickaxe", R.drawable.item_lead_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Silver Pickaxe", R.drawable.item_silver_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Tungsten Pickaxe", R.drawable.item_tungsten_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Bone Pickaxe", R.drawable.item_bone_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Gold Pickaxe", R.drawable.item_gold_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Candy Cane Pickaxe", R.drawable.item_candy_cane_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Platinum Pickaxe", R.drawable.item_platinum_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Nightmare Pickaxe", R.drawable.item_nightmare_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Deathbringer Pickaxe", R.drawable.item_deathbringer_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Reaver Shark", R.drawable.item_reaver_shark, 11, "White", 20 ,20);
-        preparePickaxes(db, "Molten Pickaxe", R.drawable.item_molten_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Cobalt Pickaxe", R.drawable.item_cobalt_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Palladium Pickaxe", R.drawable.item_palladium_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Mythril Pickaxe", R.drawable.item_mythril_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Orichalcum Pickaxe", R.drawable.item_orichalcum_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Adamantite Pickaxe", R.drawable.item_adamantite_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Titanium Pickaxe", R.drawable.item_titanium_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Pickaxe Axe", R.drawable.item_pickaxe_axe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Chlorophyte Pickaxe", R.drawable.item_chlorophyte_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Spectre Pickaxe", R.drawable.item_spectre_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Shroomite Digging Claw", R.drawable.item_shroomite_digging_claw, 11, "White", 20 ,20);
-        preparePickaxes(db, "Picksaw", R.drawable.item_picksaw, 11, "White", 20 ,20);
-        preparePickaxes(db, "Solar Flare Pickaxe", R.drawable.item_solar_flare_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Vortex Pickaxe", R.drawable.item_vortex_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Nebula Pickaxe", R.drawable.item_nebula_pickaxe, 11, "White", 20 ,20);
-        preparePickaxes(db, "Stardust Pickaxe", R.drawable.item_stardust_pickaxe, 11, "White", 20 ,20);
+        preparePickaxes(db, "Copper Pickaxe", R.drawable.item_copper_pickaxe, 4, "2","-1 range", "4%", 22,15, "White", "5 Silver" ,"1 Silver", "35%", "None");
+        preparePickaxes(db, "Tin Pickaxe", R.drawable.item_tin_pickaxe, 5, "2","None","4%",20,14,"White", "None","1 Silver 50 Copper", "35%", "None");
+        preparePickaxes(db, "Cactus Pickaxe", R.drawable.item_cactus_pickaxe, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Iron Pickaxe", R.drawable.item_iron_pickaxe, 5, "2","None", "4%",19,13,"White", "20 Silver" ,"4 Silver", "40%", "None");
+        preparePickaxes(db, "Lead Pickaxe", R.drawable.item_lead_pickaxe, 6, "2","None", "4%",18,12,"White", "None" ,"6 Silver", "43%", "None");
+        preparePickaxes(db, "Silver Pickaxe", R.drawable.item_silver_pickaxe, 6, "2","None", "4%",18,11,"White", "None" ,"10 Silver", "45%", "None");
+        preparePickaxes(db, "Tungsten Pickaxe", R.drawable.item_tungsten_pickaxe, 6, "2","None", "4%",20,19,"White", "None" ,"15 Silver", "50%", "None");
+        preparePickaxes(db, "Bone Pickaxe", R.drawable.item_bone_pickaxe, 8, "3","None", "4%",18,11,"Blue", "None" ,"30 Silver", "50%", "None");
+        preparePickaxes(db, "Gold Pickaxe", R.drawable.item_gold_pickaxe, 6, "2","None", "4%",19,17,"White", "None" ,"20 Silver", "55%", "None");
+        preparePickaxes(db, "Candy Cane Pickaxe", R.drawable.item_candy_cane_pickaxe, 7, "2.5","None", "4%",19,16,"White", "None" ,"20 Silver", "55%", "None");
+        preparePickaxes(db, "Platinum Pickaxe", R.drawable.item_platinum_pickaxe, 7, "2","None", "4%",18,15,"White", "None" ,"30 Silver", "59%", "None");
+        preparePickaxes(db, "Nightmare Pickaxe", R.drawable.item_nightmare_pickaxe, 9, "3","None", "4%",19,15,"Blue", "None" ,"36 Silver", "65%", "None");
+        preparePickaxes(db, "Deathbringer Pickaxe", R.drawable.item_deathbringer_pickaxe, 12, "3.5","None", "4%",21,14,"Blue", "None" ,"36 Silver", "70%", "None");
+        preparePickaxes(db, "Reaver Shark", R.drawable.item_reaver_shark, 16, "3","None", "4%",21,18,"Orange", "None" ,"1 Gold 50 Silver", "100%", "None");
+        preparePickaxes(db, "Molten Pickaxe", R.drawable.item_molten_pickaxe, 12, "2","Inflicts Debuff On Fire!","4%",22,15,"Orange", "None" ,"54 Silver", "100%", "None");
+        preparePickaxes(db, "Cobalt Pickaxe", R.drawable.item_cobalt_pickaxe, 5, "5","None", "4%",24,13,"Light Red", "None" ,"1 Gold 8 Silver", "110%", "None");
+        preparePickaxes(db, "Palladium Pickaxe", R.drawable.item_palladium_pickaxe, 12, "5","None", "4%",24,11,"Light Red", "None" ,"1 Gold 44 Silver", "130%", "None");
+        preparePickaxes(db, "Mythril Pickaxe", R.drawable.item_mythril_pickaxe, 15, "2","None", "4%",22,15,"White", "None" ,"1 Gold 44 Silver", "35%", "None");
+        preparePickaxes(db, "Orichalcum Pickaxe", R.drawable.item_orichalcum_pickaxe, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Adamantite Pickaxe", R.drawable.item_adamantite_pickaxe, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Titanium Pickaxe", R.drawable.item_titanium_pickaxe, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Pickaxe Axe", R.drawable.item_pickaxe_axe, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Chlorophyte Pickaxe", R.drawable.item_chlorophyte_pickaxe, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Spectre Pickaxe", R.drawable.item_spectre_pickaxe, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Shroomite Digging Claw", R.drawable.item_shroomite_digging_claw,, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Picksaw", R.drawable.item_picksaw, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Solar Flare Pickaxe", R.drawable.item_solar_flare_pickaxe, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Vortex Pickaxe", R.drawable.item_vortex_pickaxe, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Nebula Pickaxe", R.drawable.item_nebula_pickaxe, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
+        preparePickaxes(db, "Stardust Pickaxe", R.drawable.item_stardust_pickaxe, 5, "2","None", "4%",22,15,"White", "None" ,"4 Silver", "35%", "None");
     }
 
-    private void preparePickaxes(SQLiteDatabase db, String name, int picture, int damage, int knockback, String rarity, int price, int power) {
+    private void preparePickaxes(SQLiteDatabase db, String name, int picture, int damage, String knockback, String bonus, String critical_chanse, int use_time, int tool_speed, String rarity, String buy_price, String sell_price, String pickaxe_power, String axe_power) {
         ContentValues values = new ContentValues();
         values.put(NAME, name);
         values.put(PICTURE, picture);
         values.put(DAMAGE, damage);
         values.put(KNOCKBACK, knockback);
+        values.put(BONUS, bonus);
+        values.put(CRITICAL_CHANSE, critical_chanse);
+        values.put(USE_TIME, use_time);
+        values.put(TOOL_SPEED, tool_speed);
         values.put(RARITY, rarity);
-        values.put(PRICE, price);
-        values.put(POWER, power);
+        values.put(BUY_PRICE, buy_price);
+        values.put(SELL_PRICE, sell_price);
+        values.put(PICKAXE_POWER, pickaxe_power);
+        values.put(AXE_POWER, axe_power);
         db.insert(TABLE_NAME, null, values);
     }
         public void addPickaxes(Pickaxes pickaxes) {
@@ -101,10 +119,15 @@ public class PickaxesRepository {
             values.put(PICTURE, pickaxes.picture);
             values.put(DAMAGE, pickaxes.damage);
             values.put(KNOCKBACK, pickaxes.knockback);
+            values.put(BONUS, pickaxes.bonus);
+            values.put(CRITICAL_CHANSE, pickaxes.critical_chanse);
+            values.put(USE_TIME, pickaxes.use_time);
+            values.put(TOOL_SPEED, pickaxes.tool_speed);
             values.put(RARITY, pickaxes.rarity);
-            values.put(PRICE, pickaxes.price);
-            values.put(POWER, pickaxes.power);
-
+            values.put(BUY_PRICE, pickaxes.buy_price);
+            values.put(SELL_PRICE, pickaxes.sell_price);
+            values.put(PICKAXE_POWER, pickaxes.pickaxe_power);
+            values.put(AXE_POWER, pickaxes.axe_power);
             db.insert(TABLE_NAME, null, values);
             db.close();
         }
@@ -122,10 +145,16 @@ public class PickaxesRepository {
                         cursor.getString(1),
                         cursor.getInt(2),
                         cursor.getInt(3),
-                        cursor.getInt(4),
+                        cursor.getString(4),
                         cursor.getString(5),
-                        cursor.getInt(6),
-                        cursor.getInt(7)
+                        cursor.getString(6),
+                        cursor.getInt(7),
+                        cursor.getInt(8),
+                        cursor.getString(9),
+                        cursor.getString(10),
+                        cursor.getString(11),
+                        cursor.getString(12),
+                        cursor.getString(13)
                 );
 
                 pickaxe.add(pickaxes);
