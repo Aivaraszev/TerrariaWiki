f4757f0e3beee41c1971c73692ef782ee325b68c
Aivaraszev <aivaraszev@gmail.com>
2018-04-25 20:27:22 +0300

    Revert "Revert "PUSH PUSH PUSH""
    This reverts commit 7414cc593c491c72e30befb8bb6179abe6c2397c.

--- app/src/main/java/com/zevzikovas/aivaras/terraria/activities/BothSwordsActivity.java
+++ app/src/main/java/com/zevzikovas/aivaras/terraria/activities/BothSwordsActivity.java
@@ -7,19 +7,19 @@ import android.view.View;
 
 import com.zevzikovas.aivaras.terraria.R;
 
-public class WeaponsActivity extends Activity {
+public class BothSwordsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weapons);
+        setContentView(R.layout.activity_both_swords);
     }
-    public void BothSwordsActivity (View view) {
-        Intent intent = new Intent(getApplicationContext(), BothSwordsActivity.class);
+    public void HswordsList (View view) {
+        Intent intent = new Intent(getApplicationContext(), HSwordsActivity.class);
         startActivity(intent);
     }
-    public void BothYoyosActivity (View view) {
-        Intent intent = new Intent(getApplicationContext(), BothYoyosActivity.class);
+    public void SwordsList (View view) {
+        Intent intent = new Intent(getApplicationContext(), SwordsActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/zevzikovas/aivaras/terraria/activities/BothYoyosActivity.java
@@ -0,0 +1,21 @@
+package com.zevzikovas.aivaras.terraria.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+
+import com.zevzikovas.aivaras.terraria.R;
+
+public class BothYoyosActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_both_yoyos);
+    }
+    public void YoyosList (View view) {
+        Intent intent = new Intent(getApplicationContext(), YoyosActivity.class);
+        startActivity(intent);
+    }
+}
